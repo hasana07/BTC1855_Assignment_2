cd70ad3a8279ffad59d36612a60d1b2d9773e056
Trinley Palmo <t.palmo@hotmail.com>
2024-06-16 17:56:23 -0400

    Checks if user input is numeric and a three digit positive number. Added additional checks to initial plan.

--- Assignment_2_Narcissistic_numbers.R
+++ Assignment_2_Narcissistic_numbers.R
@@ -1,10 +1,15 @@
 #' Plan
 #' Step 1: Read user input
-#' Step 2: Check user input to see if it is numeric
-#'  if F
+#' Step 2: Check user input to see if it is numeric and a three digit positive 
+#'         number
+#'  if it is not numeric
 #'    Print an error message prompting user to re-run code and try again
-#'  if T
-#'    Print a message saying it is numeric.
+#'  if it is numeric but less than or equal to 99
+#'    Print an error message saying it is too small and to try again
+#'  if it is numeric but greater than or equal to 1000
+#'    Print an error message saying it is too big and to try again.
+#'  if everything is good
+#'    Print a message saying thank you.
 #' Step 3: Break down user input into a vector of digits
 #' Step 4: Sum the cubes of each of the digits.
 #' Step 5: Check if the sum is equal to the original user input
@@ -17,7 +22,27 @@
 # Prompt the user to enter a three digit positive number
 three_dig <- readline(prompt = "Please enter a three digit positive number: ")
 
-# Check if the user input is numeric. If not, print an error message and quit.
+# Convert user input into an object of type "numeric" to use in conditions below.
+three_dig_num <- as.numeric(three_dig)
+
+# If it returns TRUE for the object (as in it is NA), that means the user input 
+# was not a number.
+if (is.na(three_dig_num)) {
+  print("Your entry is not a number. Please try again.")
+  
+# We need a three digit positive number, which it means it cannot be less than 
+# or equal to 99 OR greater than or equal to 1000. If it is, a new input must 
+# be entered.
+} else if (99 >= three_dig_num) {
+  print("Your number is too small! Please try again.")
+} else if (three_dig_num >= 1000) {
+  print("Your number is too big! Please try again.")
+
+# If the user input passed all of the above conditions, we can then check if it 
+# is an Armstrong number.
+} else {
+  print("Thank you! Let's see if it is narcissistic.")
+}
 
 # Check if the number is narcissistic. A narcissistic number, or an Armstrong 
 # number, is a number that is equal to the sum of the cubes of its own digits. 
