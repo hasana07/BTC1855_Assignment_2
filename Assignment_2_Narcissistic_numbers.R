25b4f7d6d136a4588473526cfc1fc488af78edd6
Trinley Palmo <t.palmo@hotmail.com>
2024-06-16 17:14:52 -0400

    First task - prompts user input.

--- Assignment_2_Narcissistic_numbers.R
+++ Assignment_2_Narcissistic_numbers.R
@@ -15,6 +15,7 @@
 
 
 # Prompt the user to enter a three digit positive number
+three_dig <- readline(prompt = "Please enter a three digit positive number: ")
 
 # Check if the user input is numeric. If not, print an error message and quit.
 
